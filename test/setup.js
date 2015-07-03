cc135b38f380c1fed394c9f09b3fde8a10a24677
sean <seanowenhayes@gmail.com>
2015-07-03 17:17:15 +0200

    Is this on bitbucket?

---
+++ test/setup.js
@@ -0,0 +1,8 @@
+var sinon = require('sinon');
+var chai = require('chai');
+var should = chai.should();
+var nock = require('nock');
+
+global.sinon    = sinon;
+global.should   = should
+global.nock     = nock;
