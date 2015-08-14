8d93b31d3ec4cfd0ecc15dec2db82190359a3205
sean <seanowenhayes@gmail.com>
2015-08-14 23:24:05 +0200

    Turned the webpage module into a singleton.

--- test/unit/WebPageShould.js
+++ test/unit/WebPageShould.js
@@ -1,21 +1,23 @@
 'use strict';
-var WebPage = require('../../lib/WebPage');
+var webpage = require('../../lib/WebPage');
 
-describe('WebPage', function() {
+describe('webPage', function () {
+
+    afterEach(function () {
+        webpage.removeAllListeners();
+    });
 
 
-    describe('#setContent', function() {
+    describe('#setContent', function () {
 
 
-        it('should set dom property', function() {
-            var webpage = new WebPage();
+        it('should set dom property', function () {
             webpage.setContent('<h1>Hello</h1>');
             should.exist(webpage.dom);
         });
 
 
-        it('should set dom property as jquery like', function() {
-            var webpage = new WebPage();
+        it('should set dom property as jquery like', function () {
             webpage.setContent('<h1>Hello</h1>');
             var result = webpage.dom('h1').text();
             result.should.equal('Hello');
@@ -24,55 +26,54 @@ describe('WebPage', function() {
     });
 
 
-    describe('#load', function() {
+    describe('#load', function () {
 
 
-        it('should load the supplied url and call setContent', function(done) {
+        it('should load the supplied url and call setContent', function (done) {
             var target = nock('http://myapp.iriscouch.com')
                 .get('/users/1')
                 .reply(200, '<h1>reply</h1>');
-            var webpage = new WebPage();
             var setContentSpy = sinon.spy(webpage, 'setContent');
 
 
             webpage.load('http://myapp.iriscouch.com/users/1');
 
-            webpage.on('load', function() {
+            webpage.on('load', function () {
                 setContentSpy.called.should.equal(true);
+                setContentSpy.restore();
                 done();
             });
 
         });
 
 
-        it('should load the supplied url erroneousl and not call setContent', function(done) {
+        it('should load the supplied url erroneousl and not call setContent', function (done) {
             var target = nock('http://myapp.iriscouch.com')
                 .get('/users/1')
                 .reply(200, null);
-            var webpage = new WebPage();
             var setContentSpy = sinon.spy(webpage, 'setContent');
 
 
             webpage.load('http://myapp.iriscouch.com/users/1');
 
-            webpage.on('error', function() {
+            webpage.on('error', function () {
                 setContentSpy.called.should.equal(false);
+                setContentSpy.restore();
                 done();
             });
 
         });
 
 
-        it('should load the supplied url', function(done) {
+        it('should load the supplied url', function (done) {
             var target = nock('http://myapp.iriscouch.com')
                 .get('/users/1')
                 .reply(200, '<h1>reply</h1>');
-            var webpage = new WebPage();
 
 
             webpage.load('http://myapp.iriscouch.com/users/1');
 
-            webpage.on('load', function() {
+            webpage.on('load', function () {
                 target.done();
                 done();
             });
@@ -80,16 +81,15 @@ describe('WebPage', function() {
         });
 
 
-        it('should load the supplied url setting the dom property', function(done) {
+        it('should load the supplied url setting the dom property', function (done) {
             var target = nock('http://myapp.iriscouch.com')
                 .get('/users/1')
                 .reply(200, '<h1>reply</h1>');
-            var webpage = new WebPage();
 
 
             webpage.load('http://myapp.iriscouch.com/users/1');
 
-            webpage.on('load', function(webpage) {
+            webpage.on('load', function (webpage) {
                 should.exist(webpage.dom);
                 webpage.dom('h1').text().should.equal('reply');
                 target.done();
@@ -97,24 +97,23 @@ describe('WebPage', function() {
             });
 
         });
-        
-        it('should load the supplied url setting the url property', function(done) {
+
+        it('should load the supplied url setting the url property', function (done) {
             var target = nock('http://myapp.iriscouch.com')
                 .get('/users/1')
                 .reply(200, '<h1>reply</h1>');
-            var webpage = new WebPage();
 
 
             webpage.load('http://myapp.iriscouch.com/users/1');
 
-            webpage.on('load', function(webpage) {
+            webpage.on('load', function (webpage) {
                 webpage.url.should.equal('http://myapp.iriscouch.com/users/1');
                 should.exist(webpage.dom);
                 done();
             });
 
         });
-        
+
 
     });
 });
